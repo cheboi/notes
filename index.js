870095caa7338c8f062071347dd4fe5cc5beea85
cheboi <moscheb96gmail.com>
2023-02-18 10:13:02 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(express.json())
+app.listen(3000, () => console.log("listen on port 3000"))
